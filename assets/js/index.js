2ff855d755c78d4020d426b61d7d76e52b05c63c
janvipatel7 <janvi72004@gmail.com>
2025-03-17 16:58:55 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+let timer;
+let totalSeconds = 0;
+let time = document.getElementById("timer")
+
+document.getElementById("start").addEventListener("click" , function (){
+    startTimer();
+})
+
+function startTimer(){
+    let h = parseInt(document.getElementById("hours").value) || 0;
+    let m = parseInt(document.getElementById("minutes").value) || 0;
+    let s = parseInt(document.getElementById("seconds").value) || 0;
+
+    totalSeconds = (h * 3600) + (m * 60) + (s);
+
+    if(totalSeconds <= 0){
+        Swal.fire({
+            title: "Time waits for no one.",
+            text: "Include Time ...",
+            icon: "Invalide"
+          });
+    }
+
+    timer = setInterval(() => {
+        displayTime();
+        totalSeconds--;
+    } , 1000);
+}
+
+function displayTime(){
+    let hrs = Math.floor(totalSeconds / 3600);
+    let min = Math.floor((totalSeconds % 3600) / 60);
+    let sec = totalSeconds % 60;
+
+    time.innerHTML = `${hrs.toString().padStart(2, "0")} : ${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`
+}
